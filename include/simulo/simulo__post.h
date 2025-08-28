d36d294c9a353ca2b232582919b5fdbe91922a3b
Richard Grover <38992201+richgrov@users.noreply.github.com>
2025-08-27 23:19:38 -0600

    Remove cli flag for exported functions

--- include/simulo/simulo__post.h
+++ include/simulo/simulo__post.h
@@ -1,3 +1,5 @@
+#include <emscripten.h>
+
 extern "C" {
 
 static std::unique_ptr<Game> root_object;
@@ -5,6 +7,7 @@ static std::unique_ptr<Game> root_object;
 static float simulo__pose_data[17 * 2] = {0};
 static float simulo__transform_data[16] = {0};
 
+EMSCRIPTEN_KEEPALIVE
 void simulo__start() {
   kSolidTexture = std::numeric_limits<uint32_t>::max();
   simulo_set_buffers(simulo__pose_data, simulo__transform_data);
@@ -13,11 +16,13 @@ void simulo__start() {
   simulo_set_root(root_object->simulo__id, root_object.get());
 }
 
+EMSCRIPTEN_KEEPALIVE
 void simulo__update(void *ptr, float delta) {
   Object *object = static_cast<Object *>(ptr);
   object->update(delta);
 }
 
+EMSCRIPTEN_KEEPALIVE
 void simulo__recalculate_transform(void *ptr) {
   Object *object = static_cast<Object *>(ptr);
   glm::mat4 transform = object->recalculate_transform();
@@ -25,11 +30,13 @@ void simulo__recalculate_transform(void *ptr) {
               sizeof(simulo__transform_data));
 }
 
+EMSCRIPTEN_KEEPALIVE
 void simulo__pose(int id, bool alive) {
   root_object->on_pose(id, alive ? std::optional<Pose>(Pose(simulo__pose_data))
                                  : std::nullopt);
 }
 
+EMSCRIPTEN_KEEPALIVE
 void simulo__drop(void *ptr) {
   Object *object = static_cast<Object *>(ptr);
   delete object;
